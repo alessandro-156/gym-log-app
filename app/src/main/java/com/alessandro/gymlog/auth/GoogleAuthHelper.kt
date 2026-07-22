f8ea7c9f1e6f92ce9f4a1b359772c3d4d427410c
alessandro-156 <azagrebnev2007@gmail.com>
2026-07-22 18:10:35 +0500

    Create GoogleAuthHelper for handling sign-in logic

---
+++ app/src/main/java/com/alessandro/gymlog/auth/GoogleAuthHelper.kt
@@ -0,0 +1,21 @@
+package com.alessandro.gymlog.auth
+
+import android.context.Context
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+
+object GoogleAuthHelper {
+    fun getSignInClient(context: Context): GoogleSignInClient {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            // Here we will add .requestIdToken("your_client_id") later
+            .build()
+        return GoogleSignIn.getClient(context, gso)
+    }
+
+    fun getLastSigninAccount(context: Context): GoogleSignInAccount? {
+        return GoogleSignIn.getLastSignedInAccount(context)
+    }
+}
