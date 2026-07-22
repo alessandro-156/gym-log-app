50f51da986b0bfea6103d84656df1655a19b4f37
alessandro-156 <azagrebnev2007@gmail.com>
2026-07-22 19:10:08 +0500

    Finalize Google Auth configuration with Web Client ID

--- app/src/main/java/com/alessandro/gymlog/auth/GoogleAuthHelper.kt
+++ app/src/main/java/com/alessandro/gymlog/auth/GoogleAuthHelper.kt
@@ -9,8 +9,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 object GoogleAuthHelper {
     fun getSignInClient(context: Context): GoogleSignInClient {
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("496244914061-algm7tcden053bmo6f3rtcdovfr0fbh.apps.googleusercontent.com")
             .requestEmail()
-            // Here we will add .requestIdToken("your_client_id") later
             .build()
         return GoogleSignIn.getClient(context, gso)
     }
