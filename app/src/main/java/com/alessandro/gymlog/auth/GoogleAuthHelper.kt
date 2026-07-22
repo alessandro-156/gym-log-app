d405e9b69fad9ae6913c5deab9ca7c53f1a381f1
alessandro-156 <azagrebnev2007@gmail.com>
2026-07-22 18:44:19 +0500

    Fix Context import in GoogleAuthHelper.kt

--- app/src/main/java/com/alessandro/gymlog/auth/GoogleAuthHelper.kt
+++ app/src/main/java/com/alessandro/gymlog/auth/GoogleAuthHelper.kt
@@ -1,12 +1,12 @@
 package com.alessandro.gymlog.auth
 
-import android.context.Context
+import android.content.Context
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInOption
 
-object GoogleAuthHelper {
+object GoogleAuthHeller {
     fun getSignInClient(context: Context): GoogleSignInClient {
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestEmail()
@@ -16,6 +16,6 @@ object GoogleAuthHelper {
     }
 
     fun getLastSigninAccount(context: Context): GoogleSignInAccount? {
-        return GoogleSignIn.getLastSignedInAccount(context)
+        return GoogleSignIn.getLastSignedAccount(context)
     }
 }
